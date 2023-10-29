ae4ee2834dcdeca7aa7a684125acf6888462ae2c
cobluestars <cobaltbluestars@gmail.com>
2023-10-29 11:29:03 +0900

    Ver 1.0.6
    .ts .tsx, .js, jsx 컴파일에 포함하는 로직 추가 보강

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // HTML 파일을 처리하기 위한 플러그인
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.js', // 진입점 파일 설정
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: 'bundle.js', // 출력 파일 이름
+    path: path.resolve(__dirname, 'dist') // 출력 경로
   },
   module: {
     rules: [
@@ -15,30 +15,29 @@ module.exports = {
             use: {
                 loader: 'babel-loader',
                 options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react']
+                    presets: ['@babel/preset-env', '@babel/preset-react'] // Babel 설정
                 }
             }
         },
         {
             test: /\.(ts|tsx)$/,
             exclude: /node_modules/,
-            use: 'ts-loader'
+            use: 'ts-loader' // TypeScript 설정
         }
     ]
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']   
+    extensions: ['.js', '.jsx', '.ts', '.tsx']   // 처리할 파일 확장자 목록
   },
-  devtool: 'source-map', // 소스맵 활용
+  devtool: 'source-map', // 소스 맵 옵션 활성화
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 9000
   },
-  plugins: [ // 플러그인 섹션
+  plugins: [ 
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html' // HTML 템플릿 위치
     })
   ]
 };
-
