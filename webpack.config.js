5213d7fa96d51f1c82e3a9ccb005c088c32cba12
cobluestars <cobaltbluestars@gmail.com>
2023-11-05 18:24:51 +0900

    Ver 2.2.1
    Webpack 관련 설정

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     filename: 'QuickRenard.js', // 출력 파일 이름
     path: path.resolve(__dirname, 'dist'), // 출력 경로
-    library: 'QuickRenard', // 라이브러리 이름
+    library: 'quickrenard', // 라이브러리 이름
     libraryTarget: 'umd', // Universal Module Definition
     globalObject: 'this' // 글로벌 객체 설정
   },
@@ -25,17 +25,22 @@ module.exports = {
             use: {
                 loader: 'babel-loader',
                 options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react'] // Babel 설정
+                    presets: ['@babel/preset-env', '@babel/preset-react'] // Javascript Babel 설정
                 }
             }
         },
         {
-            test: /\.(ts|tsx)$/,
-            exclude: /node_modules/,
-            use: 'ts-loader' // TypeScript 설정
-        }
-    ]
-  },
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'] // TypeScript Babel 설정
+            }
+          }
+        },
+      ]
+    },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx']   // 처리할 파일 확장자 목록
   },
